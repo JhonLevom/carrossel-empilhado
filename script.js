1525c4c0dac6a9a5fb5b7a0035eed74f00cb1505
JonasJesus <jhonlevom@outlook.com>
2021-08-23 21:10:22 -0300

    adicionando as imagens em um array

--- script.js
+++ script.js
@@ -4,35 +4,37 @@ var  img3 =  "./img3.webp"
 var  img4 =  "./img4.webp"
 var  img5 =  "./img5.webp"
 
+const images = [img1, img2, img3, img4, img5]
+
 var contador = 0
 
 
 function trocaDeImagem(){
     
     if(contador === 0 ){
-        imagem1 = img1
-        imagem2 = img2
-        imagem3 = img3
+        imagem1 = images[0]
+        imagem2 = images[1]
+        imagem3 = images[2]
     }
     if(contador === 1){
-        imagem1 = img2
-        imagem2 = img3
-        imagem3 = img4       
+        imagem1 = images[1]
+        imagem2 = images[2]
+        imagem3 = images[3]      
     }
     if(contador === 2){
-        imagem1 = img3
-        imagem2 = img4
-        imagem3 = img5        
+        imagem1 = images[2]
+        imagem2 = images[3] 
+        imagem3 = images[4]         
     }
     if(contador === 3){
-        imagem1 = img4
-        imagem2 = img5
-        imagem3 = img1        
+        imagem1 = images[3] 
+        imagem2 = images[4] 
+        imagem3 = images[5]        
     }
     if(contador === 4){
-        imagem1 = img5
-        imagem2 = img1
-        imagem3 = img2       
+        imagem1 = images[4]
+        imagem2 = images[5]
+        imagem3 = images[5]       
     }
     console.log(contador)
     nextImage()
